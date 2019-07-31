6c12c9ea27f5095f93221edf48cc3fc7905a8289
Hai Nguyen <hainm.comp@gmail.com>
2019-07-31 17:10:53 -0400

    typescript

--- js/src/widget.ts
+++ js/src/widget.ts
@@ -6,8 +6,9 @@ import {Fullscreen} from "./fullscreen"
 // https://github.com/arose/nglview/blob/7ba64def232a246c68fc96d059600b4c671703c5/js/src/widget_ngl.js#L1115
 // The MIT License, Copyright (c); 2015 Alexander S. Rose
 
-var FullscreenModel = DOMWidgetModel.extend({
-  defaults: function() {
+export
+class FullscreenModel extends DOMWidgetModel{
+  defaults(){
     return _.extend(DOMWidgetModel.prototype.defaults(), {
       _model_name: "FullscreenModel",
       _model_module: "fullscreen-js-widgets",
@@ -17,19 +18,24 @@ var FullscreenModel = DOMWidgetModel.extend({
       _view_module_version: require("../package.json").version
     });
   }
-});
+}
 
-var FullscreenView = DOMWidgetView.extend({
-  render: function() {
+
+export
+class FullscreenView extends DOMWidgetView{
+  fs: Fullscreen
+
+  render(){
+    console.log('there')
     this.fs = new Fullscreen();
     var that = this;
     this.model.on("msg:custom", function(msg) {
       that.on_msg(msg);
     });
     this.handleSignals();
-  },
+  }
 
-  fullscreen: function(model_id) {
+  fullscreen(model_id){
     var that = this;
     this.model.widget_manager.get_model(model_id).then(model => {
       var key = Object.keys(model.views)[0];
@@ -37,28 +43,23 @@ var FullscreenView = DOMWidgetView.extend({
         that.fs.toggleFullscreen(view.el);
       });
     });
-  },
+  }
 
-  handleSignals: function() {
+  handleSignals(){
     var that = this;
     this.fs.signals.fullscreenChanged.add(function(isFullscreen) {
       that.model.set("_is_fullscreen", isFullscreen);
       that.touch();
     });
-  },
+  }
 
-  execute_code: function(code) {
+  execute_code(code){
     eval(code);
-  },
+  }
 
-  on_msg: function(msg) {
+  on_msg(msg){
     if ("execute_code" in msg) {
       this.execute_code(msg.execute_code);
     }
   }
-});
-
-module.exports = {
-  FullscreenModel: FullscreenModel,
-  FullscreenView: FullscreenView
-};
+}
